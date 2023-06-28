76dd94cf6d0e46638a57a0a13fb0ec4035d60089
malkootkhan <engr.mkhan1990@gmail.com>
2023-06-28 18:39:04 +0100

    Update Main.c

--- Main.c
+++ Main.c
@@ -1,3 +1,6 @@
+/* Please be aware that the code in its current state is in the initial phase and has undergone minimal testing. It is strongly recommended to thoroughly test and make any necessary modifications before utilizing it. */
+/* I cannot be held responsible for any issues or difficulties arising from using the code without making appropriate modifications. */
+
 #include <stdio.h>
 #include <stdlib.h>
 
